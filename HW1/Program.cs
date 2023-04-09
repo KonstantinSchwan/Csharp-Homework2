45ad2960ca1b01a5796e3678860cda46e04e4456
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-09 16:17:04 +0200

    Задание готово

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int GetSecondDigit(int a)
+{
+    if((a >= 100 && a <= 999) || (a <= -100 && a >= -999))
+        return (a % 100) / 10;
+    else
+        return -1;
+}
+
+Console.WriteLine("Введите число:");
+
+int a = int.Parse(Console.ReadLine());
+
+int result = GetSecondDigit(a);
+
+if (result == -1)
+    Console.WriteLine("Число не трёзначное");
+else
+    Console.WriteLine(result);
+    
