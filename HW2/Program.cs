f0a632260db77c4e197f6571bb5c25ddfa1cda57
KonstantinSchwan <konstantinschwan@gmx.de>
2023-04-19 16:14:38 +0200

    Задача 13 решина

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -9,4 +9,12 @@
 
 
 
-Console.WriteLine("Введите число: ");
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
